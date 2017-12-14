8ea44ead3951b40566c7a5ecd0770a991e6dbbdc
Adamka <yehveeen@gmail.com>
2017-12-14 15:49:34 +0100

    added checktask function

--- Todo App/src/Main.java
+++ Todo App/src/Main.java
@@ -22,7 +22,7 @@ public class Main {
             removeTasks(args);
         }
         else if (args.length != 0 && args[0].equals("-c")){
-
+            checkTasks (args);
         }
 
     }
@@ -42,11 +42,12 @@ public class Main {
         try {
             Path listPath = Paths.get("D:/Greenfox/Greenfox2/yehven_handle-todo-app/Todo App/src/my-file.txt");
             List<String> lines = Files.readAllLines(listPath);
-            int taskNumber = 1;
+            int taskNumber = 0;
             if (lines.get(0) != null) {
                 for (int i = 0; i < lines.size(); i++) {
-                    System.out.println(taskNumber + " - " + lines.get(i));
                     taskNumber++;
+                    System.out.println(taskNumber + " - " + lines.get(i));
+
                 }
             }
             else {
@@ -77,7 +78,7 @@ public class Main {
             if (lines.size() < taskLineNumber) {
                 System.out.println("Unable to remove from file: index out of bound");
             } else if (lines.size() >= 2 && lines.size() > taskLineNumber) {
-                lines.remove(taskLineNumber - 1);
+                lines.remove(taskLineNumber -1);
             }
             Files.write(listPath, lines, Charset.defaultCharset());
 
@@ -87,8 +88,51 @@ public class Main {
             System.out.println("Unable to remove from file: no index provided");
         }
     }
+    public static void checkTasks (String[] args) {
+        try {
+            Path listPath = Paths.get("D:/Greenfox/Greenfox2/yehven_handle-todo-app/Todo App/src/my-file.txt");
+            List<String> lines = Files.readAllLines(listPath);
 
+            int argLineNumber = Integer.parseInt(args[1]);
+            int lineNr = 0;
 
+            if (argLineNumber > lines.size()) {
+                System.out.println("Unable to check: index is out of bound");
+            }
+
+            for (int i=0; i < lines.size(); i++) {
+                if (!lines.get(i).startsWith("[ ]") && !lines.get(i).startsWith("[X]") ) {
+                    String buffer1 = lines.get(i);
+                    lines.remove(i);
+                    lines.add(i, "[ ]" + buffer1);
+                }
+            }
+
+            for (int i=0; i < lines.size(); i++) {
+                if (lines.get(i).startsWith("[X]")) {
+                    String buffer = lines.get(i);
+                    lines.remove(i);
+                    lines.add(i, buffer);
+                    lineNr++;
+                } else if (lines.get(i).startsWith("[ ]") && (lineNr != argLineNumber -1)) {
+                    String buffer = lines.get(i);
+                    lines.remove(i);
+                    lines.add(i, buffer);
+                    lineNr++;
+                } else if (lines.get(i).startsWith("[ ]") && (lineNr == argLineNumber -1)) {
+                    String buffer = lines.get(i).substring(3);
+                    lines.remove(i);
+                    lines.add(i, "[X]" + buffer);
+                    lineNr++;
+                }
+            }
+            Files.write(listPath, lines, Charset.defaultCharset());
+        } catch (NumberFormatException notANumber) {
+            System.out.println("Unable to remove: index not a number");
+        } catch (Exception noIndex) {
+            System.out.println("Unable to remove: no index provided");
+        }
+    }
 
 
 }
