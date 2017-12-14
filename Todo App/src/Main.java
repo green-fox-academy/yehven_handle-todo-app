00d77a707f24c32c0a296a5acda1f58d5f7e71db
Adamka <yehveeen@gmail.com>
2017-12-14 14:56:39 +0100

    added Todo App until remove function

---
+++ Todo App/src/Main.java
@@ -0,0 +1,94 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            printUsage();
+        }
+        else if (args.length != 0 && args[0].equals("-l")){
+            listTasks();
+        }
+        else if (args.length != 0 && args[0].equals("-a")){
+            addTasks(args);
+        }
+        else if (args.length != 0 && args[0].equals("-r")){
+            removeTasks(args);
+        }
+        else if (args.length != 0 && args[0].equals("-c")){
+
+        }
+
+    }
+
+    private static void printUsage() {
+        System.out.println("Command Line Todo application\n" +
+                "=============================\n" +
+                "\n" +
+                "Command line arguments:\n" +
+                " -l   Lists all the tasks\n" +
+                " -a   Adds a new task\n" +
+                " -r   Removes an task\n" +
+                " -c   Completes an task");
+    }
+
+    private static void listTasks (){
+        try {
+            Path listPath = Paths.get("D:/Greenfox/Greenfox2/yehven_handle-todo-app/Todo App/src/my-file.txt");
+            List<String> lines = Files.readAllLines(listPath);
+            int taskNumber = 1;
+            if (lines.get(0) != null) {
+                for (int i = 0; i < lines.size(); i++) {
+                    System.out.println(taskNumber + " - " + lines.get(i));
+                    taskNumber++;
+                }
+            }
+            else {
+                System.out.println("No todos for today! :)");
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to read the file.");
+        }
+    }
+
+    private static void addTasks(String args[]){
+        try {
+            Path listPath = Paths.get("D:/Greenfox/Greenfox2/yehven_handle-todo-app/Todo App/src/my-file.txt");
+            List<String> lines = Files.readAllLines(listPath);
+            lines.add(args [1]);
+            Files.write(listPath, lines);
+        } catch (Exception e) {
+            System.out.println("Unable to add to file");
+        }
+    }
+
+    public static void removeTasks(String[] args) {
+        try {
+            Path listPath = Paths.get("D:/Greenfox/Greenfox2/yehven_handle-todo-app/Todo App/src/my-file.txt");
+            List<String> lines = Files.readAllLines(listPath);
+            int taskLineNumber = Integer.parseInt(args[1]);
+
+            if (lines.size() < taskLineNumber) {
+                System.out.println("Unable to remove from file: index out of bound");
+            } else if (lines.size() >= 2 && lines.size() > taskLineNumber) {
+                lines.remove(taskLineNumber - 1);
+            }
+            Files.write(listPath, lines, Charset.defaultCharset());
+
+        } catch (NumberFormatException notANumber) {
+            System.out.println("Unable to remove from file: index not a number");
+        } catch (Exception noIndex) {
+            System.out.println("Unable to remove from file: no index provided");
+        }
+    }
+
+
+
+
+}
